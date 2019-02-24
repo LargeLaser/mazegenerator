7ddab11397f95a9514f21324281056f97eac066c
LargeLaser <47934777+LargeLaser@users.noreply.github.com>
2019-02-23 20:59:59 -0800

    Add files via upload

---
+++ Cell.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections.Generic;
+
+namespace LargeLaser
+{
+    // contains state of one location in maze.
+    public class Cell
+    {
+        // what this cell looks like.
+        public enum Type
+        {
+            None,
+            Corridor,
+            Corner,
+            Tee,
+            Cross,
+            End
+        }
+
+        // some gamey state we can apply to this location.
+        public enum Action
+        {
+            None,
+            Encounter,
+            Chest,
+            Exit
+        }
+
+        // defines where we can move to from this cell.
+        public List<Point> Links;
+
+        // what this cell looks like.
+        public Type CellType;
+
+        // gamey state, what happens when we enter this cell.
+        public Action ActionType;
+
+        // orientation of this cell.
+        public float Angle;
+
+        // gamey state, have we been here before.
+        public int Visited;
+
+        public Cell()
+        {
+            Links = new List<Point>();
+            CellType = Type.None;
+            ActionType = Action.None;
+        }
+
+        public Cell(int x, int y, List<Link> links)
+        {
+            Links = new List<Point>();
+            CellType = Type.None;
+            ActionType = Action.None;
+
+            foreach (Link link in links)
+            {
+                if (link.P1.x == x && link.P1.y == y)
+                {
+                    Links.Add(new Point(link.P2.x, link.P2.y));
+                }
+                else if (link.P2.x == x && link.P2.y == y)
+                {
+                    Links.Add(new Point(link.P1.x, link.P1.y));
+                }
+            }
+        }
+
+        public void makeExit(int x, int y)
+        {
+            ActionType = Action.Exit;
+            Links.Add(new Point(x, y - 1));
+        }
+
+        // for querying if we can move to the position in args, from this cell.
+        public bool hasLink(int x, int y)
+        {
+            foreach (Point p in Links)
+            {
+                if (p.x == x && p.y == y)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ Link.cs
@@ -0,0 +1,25 @@
+﻿namespace LargeLaser
+{
+    // simple library agnostic link between 2 positions.
+    public class Link
+    {
+        public Point P1;
+        public Point P2;
+
+        public Link(Point p1, Point p2)
+        {
+            P1 = p1;
+            P2 = p2;
+        }
+
+        public bool hasLink(int x1, int y1, int x2, int y2)
+        {
+            if ((P1.x == x1 && P1.y == y1 && P2.x == x2 && P2.y == y2) ||
+                (P2.x == x1 && P2.y == y1 && P1.x == x2 && P1.y == y2))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
